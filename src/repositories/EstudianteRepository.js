43d2766850238fcff23cc4e8eef84a0fd634df24
Dency503 <dennisgodinez64@gmail.com>
2024-10-20 16:24:55 -0600

    Mantenimiento de estudiantes

--- src/repositories/EstudianteRepository.js
+++ src/repositories/EstudianteRepository.js
@@ -7,5 +7,13 @@ module.exports={
         } catch (error) {
             console.log('Ocurrio un problema al consultar la lista de estudiantes: ',error)
         }
-    }
+    },
+    eliminarEstudiante: async(idestudiante) => { 
+        try{ 
+          const result = await pool.query('DELETE FROM estudiantes WHERE idestudiante = ?', [idestudiante]); 
+          return result.affectedRows > 0; 
+        }catch(error){ 
+          console.error('Erro al eliminar el registro', error); 
+        } 
+    } 
 }
